bc0462c8eb8ed18bfa9b697c5f07a0cb73a08dca
Gustavo <guatarashi@gmail.com>
2023-06-02 16:53:47 -0300

    feat: Implementado método para gerar o número da conta pelo mapstruct.

--- src/main/kotlin/br/com/exactaworks/exactabank/mapper/AccountMapper.kt
+++ src/main/kotlin/br/com/exactaworks/exactabank/mapper/AccountMapper.kt
@@ -5,12 +5,27 @@ import br.com.exactaworks.exactabank.controller.response.AccountResponse
 import br.com.exactaworks.exactabank.repository.entity.Account
 import org.mapstruct.Mapper
 import org.mapstruct.Mapping
+import org.mapstruct.Mappings
 
 @Mapper(componentModel = "spring")
-interface AccountMapper {
+abstract class AccountMapper {
 
-    @Mapping(target = "id", expression = "java(java.util.UUID.randomUUID().toString())")
-    fun requestToAccount(accountRequest: AccountRequest): Account
+    @Mappings(
+        Mapping(target = "id", expression = "java(java.util.UUID.randomUUID().toString())"),
+        Mapping(target = "accountNumber", expression = "java(generatedAccountNumber())")
+        )
+    abstract fun requestToAccount(accountRequest: AccountRequest): Account
 
-    fun accountToResponse(account: Account): AccountResponse
+    abstract fun accountToResponse(account: Account): AccountResponse
+
+    fun generatedAccountNumber(): String {
+        val allowedCharacters = ('A'..'Z')
+        val allowedNumbers = ('0'..'9')
+
+        val firstCharacter = allowedCharacters.random()
+        val numbers = (1..4).map { allowedNumbers.random() }.joinToString("")
+        val lastCharacter = allowedCharacters.random()
+
+        return "$firstCharacter$numbers$lastCharacter"
+    }
 }
--- src/main/kotlin/br/com/exactaworks/exactabank/repository/entity/Account.kt
+++ src/main/kotlin/br/com/exactaworks/exactabank/repository/entity/Account.kt
@@ -2,7 +2,7 @@ package br.com.exactaworks.exactabank.repository.entity
 
 data class Account(
     var id: String,
-    val accountNumber: String?,
+    val accountNumber: String,
     val balance: Double,
     val name: String,
     val email: String,
